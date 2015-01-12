06e09f7c7db941accc86f06d309ea553043238df
Zzortell <remi.blaise@gmx.fr>
2015-01-12 15:57:10 +0100

    Delete useless stuff

--- Validator/Constraints/CssNameColorValidator.php
+++ Validator/Constraints/CssNameColorValidator.php
@@ -2,20 +2,7 @@
 
 namespace Zz\ValidationExtraBundle\Validator\Constraints;
 
-use Symfony\Component\Validator\Constraint,
-	Zz\ValidationExtraBundle\Validator\ValidateCheckingIsValidMethod;
-
 class CssNameColorValidator extends HtmlNameColorValidator
 {
-	use ValidateCheckingIsValidMethod;
-	
 	protected $colors = [ 'aliceblue','antiquewhite','aquamarine','azure','beige','bisque','blanchedalmond','blueviolet','brown','burlywood','cadetblue','chartreuse','chocolate','coral','cornflowerblue','cornsilk','crimson','cyan','darkblue','darkcyan','darkgoldenrod','darkgray','darkgreen','darkkhaki','darkmagenta','darkolivegreen','darkorange','darkorchid','darkred','darksalmon','darkseagreen','darkslateblue','darkslategray','darkturquoise','darkviolet','deeppink','deepskyblue','dimgray','dodgerblue','firebrick','floralwhite','forestgreen','gainsboro','ghostwhite','gold','goldenrod','greenyellow','honeydew','hotpink','indianred','indigo','ivory','khaki','lavender','lavenderblush','lawngreen','lemonchiffon','lightblue','lightcoral','lightcyan','lightgoldenrodyellow','lightgray','lightgreen','lightpink','lightsalmon','lightseagreen','lightskyblue','lightslategray','lightsteelblue','lightyellow','limegreen','linen','magenta','mediumaquamarine','mediumblue','mediumorchid','mediumpurple','mediumseagreen','mediumslateblue','mediumspringgreen','mediumturquoise','mediumvioletred','midnightblue','mintcream','mistyrose','moccasin','navajowhite','oldlace','olivedrab','orangered','orchid','palegoldenrod','palegreen','paleturquoise','palevioletred','papayawhip','peachpuff','peru','pink','plum','powderblue','rosybrown','royalblue','saddlebrown','salmon','sandybrown','seagreen','seashell','sienna','skyblue','slateblue','slategray','snow','springgreen','steelblue','tan','thistle','tomato','turquoise','violet','wheat','whitesmoke','yellowgreen' ];
-	
-	public function isValid ( $value, Constraint $constraint ) {
-		if ( in_array(strtolower($value), $this->colors) )
-		{
-			return true;
-		}
-		return false;
-	}
 }
